aca0740da2cc507b153684b901ab805d97103916
Bruno Salerno <salerno@kth.se>
2019-11-15 16:53:27 +0100

    Add actual simulation code

--- sumo_parallel.py
+++ sumo_parallel.py
@@ -1,28 +1,58 @@
 import multiprocessing
 import traci
+import numpy as np
 
 def simulation(cmd):
     traci.start(cmd)
     i = 1
-    laststep = 1800
+    laststep = 36001
+    minutes = 0
+
+    sim_speeds = []
+    departed = 0
 
     while i <= laststep:
         traci.simulationStep()
-        IDList = traci.vehicle.getIDList()
-        i += 1
-        if len(IDList):
-            print IDList
+
+        departed += traci.simulation.getDepartedNumber()
+
+        if i % 600 == 0:
+            minutes += 1
+
+            # Warm-up check (15 min threshold)
+            if minutes > 15:
+                vehicles_ids = traci.vehicle.getIDList()
+                speeds = []
+                for vid in vehicles_ids:
+                    speeds.append(traci.vehicle.getSpeed(vid))
+
+                speed = np.mean(speeds)
+                var   = np.var(speeds)
+                sim_speeds.append(speed)
         
+        i += 1
+ 
     traci.close()
+
+    mean_speed = np.mean(sim_speeds)
+    var_speed = np.var(sim_speeds)
+    print("Mean speed: {}".format(mean_speed))
+    print("Variance: {}".format(var_speed))
+    print("Departed: {}".format(departed))
 
 if __name__ == "__main__":
 
     sumo_exe = "sumo"
     conf_file = "config.xml"  # the configuration file
-    sumo_cmd = [sumo_exe, "-c", conf_file]
+    sumo_cmd = [sumo_exe, "-c", conf_file, '--random']
 
     procs = []
-    max_process = multiprocessing.cpu_count()
+    n_processors = multiprocessing.cpu_count()
+    max_process = n_processors
+
+    print("{} processors".format(n_processors))
+    print("{} processes".format(max_process))
+
     for i in range(1, max_process):
         proc = multiprocessing.Process(target=simulation, args=(sumo_cmd,))
         procs.append(proc)
