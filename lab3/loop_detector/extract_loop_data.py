0a78860b435da5f06215f8535bbdc4e30659f2bf
Bruno Salerno <br.salerno@gmail.com>
2019-11-25 15:00:46 +0100

    Lab3: improve detector data extraction script

--- lab3/loop_detector/extract_loop_data.py
+++ lab3/loop_detector/extract_loop_data.py
@@ -9,38 +9,45 @@ import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import collections
 import sys
-
-""" 
+import numpy as np
+"""
 Extracting flow from lane 0 in link 4:
 """
 
 
-def flow(loopId,xml,feature):
+def extractMeanFeature(xml,feature):
     tree = ET.parse(xml)
     root = tree.getroot()
     time_flow = collections.OrderedDict()  # Ordered dictionary containing time periods as keys and flows in lane 0 in link 4 as values
-    for information in root.findall("interval"):
-        if information.get("id") == loopId:
-            end = float(information.get("end"))
-            flow = float(information.get(feature))
-            time_flow[int(end / 60)] = int(flow)
+
+    detectors = ['il1', 'il2', 'il3']
+    for detector in detectors:
+        for information in root.findall("interval"):
+            if information.get("id") == detector:
+                end = float(information.get("end"))
+                if (end / 60) > 15: # warmup check
+                    val = float(information.get(feature))
+                    time_flow[int(end / 60)] = val
+
+    for t in time_flow:
+        time_flow[t] = np.mean(time_flow[t])
+
     return time_flow
 
-def extract_and_plot(xml, feature):
-    loops = ['il1', 'il2', 'il3']
-
-    for loopId in loops:
-        data = flow(loopId, xml, feature)
-        time_range = list(data.keys())
-        values = list(data.values())
-        plt.plot(time_range, values, label=feature + ' ' + loopId)
-        plt.xlabel('Duration (minutes)')
+def extractAndPlot(xml, feature):
+    data = extractMeanFeature(xml, feature)
+    time_range = list(data.keys())
+    values = list(data.values())
+    print(values)
+    plt.plot(time_range, values, label=feature)
+    plt.xlabel('Duration (minutes)')
+    plt.ylabel('Speed (m/s)')
 
 if __name__ == "__main__":
     file = sys.argv[1]
-    extract_and_plot(file, 'speed')
+    extractAndPlot(file, 'speed')
 
-    extract_and_plot(file, 'harmonicMeanSpeed')
+    extractAndPlot(file, 'harmonicMeanSpeed')
 
     plt.grid(True)
     plt.legend()
