419dc864a3b7ea4f377e6727d5f3de915e4155e3
Bruno Salerno <br.salerno@gmail.com>
2019-12-02 13:41:21 +0100

    Rename ctm class

--- lab3/ctm/ctm.py
+++ lab3/ctm/ctm.py
@@ -1,7 +1,8 @@
-class CTM:
-    def __init__(self, c, tau, v0, p_m, m):
+class Iteration:
+    def __init__(self, c, tau, v0, p_m, m, q_a):
         self._q = []
         self._c = c
+        self._q_a = q_a
         self.tau = tau
         self.v0 = v0
         self.p_m = p_m
@@ -34,7 +35,7 @@ class CTM:
     def q1(self, k):
         return min(d(k),s(k+1))
 
-    def p_a(self, q, t):
+    def p_a_next(self, k):
 
     def q_e(self, p):
         if p <= self.pc():
@@ -42,5 +43,8 @@ class CTM:
         else
             return 1/self.tau * (1 - p/self.p_m)
 
-    def q_a(self, q, t):
-        return self.m * self.q_e(self.p_a(t+1)/self.m) # p_a(t+1) ?
+    def q_a(self, k):
+        return self._q_a[k]
+
+    def q_a_next(self, k):
+        return self.m * self.q_e(self.p_a_next(k)/self.m) # p_a(t+1) ?
