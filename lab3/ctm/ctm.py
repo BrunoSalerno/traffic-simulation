053168a3998be46c800227b56ab127ff4d4a8e94
Bruno Salerno <br.salerno@gmail.com>
2019-12-02 13:27:42 +0100

    Update ctm

--- lab3/ctm/ctm.py
+++ lab3/ctm/ctm.py
@@ -1,7 +1,14 @@
 class CTM:
-    def __init__(self, c):
+    def __init__(self, c, tau, v0, p_m, m):
         self._q = []
         self._c = c
+        self.tau = tau
+        self.v0 = v0
+        self.p_m = p_m
+        self.m = m
+
+    def pc(self):
+        return 1/(self.v0 * self.tau + 1 / self.p_m)
 
     def q(self, k):
         return self._q[k]
@@ -10,13 +17,13 @@ class CTM:
         return self._c[k]
 
     def s(self, k):
-        if qa(k) > pc:
-            return q(k)
+        if self.q_a(k) > self.pc():
+            return self.q_a(k)
         else
-            return c(k)
+            return self.c(k)
 
     def d(self, k):
-        if (q(k) <= pc:
+        if (q(k) <= this.pc:
             return q(k)
         else:
             return c(k)
@@ -26,3 +33,14 @@ class CTM:
 
     def q1(self, k):
         return min(d(k),s(k+1))
+
+    def p_a(self, q, t):
+
+    def q_e(self, p):
+        if p <= self.pc():
+            return self.v0 * p
+        else
+            return 1/self.tau * (1 - p/self.p_m)
+
+    def q_a(self, q, t):
+        return self.m * self.q_e(self.p_a(t+1)/self.m) # p_a(t+1) ?
