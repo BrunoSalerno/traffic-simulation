cc847de54bb5b0ba95711ae1b4002d4964c7cb54
Bruno Salerno <br.salerno@gmail.com>
2019-12-04 16:05:25 +0100

    Fix model

--- lab3/ctm/ctm.py
+++ lab3/ctm/ctm.py
@@ -16,8 +16,10 @@ class Edge:
     def q_e(self, p):
         if p <= self.pc():
             return self.v0 * p
-        else:
+        elif p > self.pc() and p <= self.p_m:
             return 1/self.tau * (1 - p/self.p_m)
+        else:
+            return 0
 
     def s(self):
         if self.q_a > self.pc():
@@ -34,7 +36,7 @@ class Edge:
     def q0(self):
         return min(self.d_prevk,self.s())
 
-    def p_a_next(self, q1, delta_x = 1, delta_t = 5 / 60):
+    def p_a_next(self, q1, delta_x = 1, delta_t = 5.0 / 60):
         return self.p_a + 1/delta_x * (self.q0() - q1) * delta_t
 
     def q_a_next(self, q1):
@@ -48,13 +50,13 @@ class Simulation:
         self.n_iters = n_iters
 
     def run(self):
-        v0 = 40
-        p_m = 50
-        q_a = 3000
-        c = 5000
-        c_nextk = 5000
-        d_prevk = 10
+        v0 = 40.0
+        p_m = 100.0
+        c = 3000.0
+        d_prevk = 3000.0
+
         p_a = 40
+        q_a = 3000
 
         output = {}
 
@@ -62,26 +64,30 @@ class Simulation:
         for i in range(self.n_iters):
             edges_data = []
             for e in range(self.edges):
+                if e == 0:
+                    p_a = 40
+    
                 prev_edge = edges_data[-1] if e > 0 else None
                 edge_tminus1 = iterations[-1][e] if i > 0 else None 
                 prev_edge_tminus1 = iterations[-1][e-1] if i > 0 and e > 0 else None
 
-                edge = Edge(self.tau, v0, p_a, p_m, self.m, q_a, c, c_nextk, d_prevk)
+                edge = Edge(self.tau, v0, p_a, p_m, self.m, q_a, c, c, d_prevk)
 
                 if prev_edge and edge_tminus1:
                     q0 = prev_edge.q0()
                     q1 = edge.q0()
                     q1_tminus1 = edge_tminus1.q0()
-                    q_a = prev_edge_tminus1.q_a_next(q1_tminus1)
+                    next_q_a = prev_edge_tminus1.q_a_next(q1_tminus1)
+                    if next_q_a > 0:
+                        q_a = next_q_a
+                    
                     p_a = prev_edge_tminus1.p_a_next(q1_tminus1)
 
                     if not e in output:
                         output[e] = []
-                    output[e].append({'p_a':p_a,'q0':q0,'q1':q1})
+                    output[e].append({'p_a':p_a,'q0':q0,'q1':q1,'d': prev_edge.d(), 's': prev_edge.s(), 'q_a':q_a})
 
                     d_prevk = prev_edge.d()
-
-                c_nextk = edge.c
 
                 edges_data.append(edge) #= np.append(edges_data, edge)
             iterations.append(edges_data)# = np.append(iterations, edges_data)
--- lab3/ctm/main.py
+++ lab3/ctm/main.py
@@ -7,10 +7,23 @@ import pandas as pd
 
 from ctm import Simulation
 
+def plot_intervals(data, var, edges=None):
+    for edge_i in data:
+        if not edges or (edge_i in edges):
+            ys = [e[var] for e in data[edge_i]][0:]
+            print('edge {}'.format(edge_i), var,ys)
+            plt.plot(ys, label='{} edge {}'.format(var, edge_i))
+        
+
+def plot_interval(interval, data, var):
+    ys =  [data[edge][interval][var] for edge in data]
+    plt.plot(ys, label='Interval {}, {}'.format(interval, var))
+
+
 if __name__ == '__main__':
     edges = 5
-    m = 3
-    tau = 1
+    m = 3.0
+    tau = 0.001
     n_iters = 30
 
     sim = Simulation(edges, m, tau, n_iters)
@@ -18,10 +31,16 @@ if __name__ == '__main__':
 
     print(output)
 
-    for edge_i in output:
-        ys = [e['q0'] for e in output[edge_i]]
-        ys = ys[3:]
-        plt.plot(ys, label='q0 edge {}'.format(edge_i))
-
+    #plot_intervals(output,'q0')
+    plot_intervals(output,'q_a',[2])
+    #plot_intervals(output,'p_a')
+    #plot_intervals(output,'q_a')
+    #plot_intervals(output,'s')
+    #plot_intervals(output,'d')
+    #for i in range(n_iters-1):
+    #plot_interval(5, output, 'q0')
+    #plot_interval(6, output, 'q0') 
+    #plot_interval(7, output, 'q0') 
     plt.legend()
     plt.show()
+
