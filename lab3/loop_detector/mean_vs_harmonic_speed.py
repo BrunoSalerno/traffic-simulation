d91799bdb4a70566ec22b2a45485cba407343bf6
Bruno Salerno <br.salerno@gmail.com>
2019-12-06 16:17:46 +0100

    Update loop detector

--- lab3/loop_detector/mean_vs_harmonic_speed.py
+++ lab3/loop_detector/mean_vs_harmonic_speed.py
@@ -11,6 +11,21 @@ import collections
 import sys
 import numpy as np
 
+def fetch_edge_attrs(data, edge_id, attrs):
+    values = {}
+    intervals = data.findall('interval')
+    for interval in intervals:
+        end = float(interval.get('end'))
+        if (end / 60) >= 15: # warmup check
+            for edge in interval:
+                if edge.get('id') == edge_id:
+                    for attr in attrs:
+                        if not attr in values:
+                            values[attr] = []
+                        if edge.get(attr) is not None:
+                            values[attr].append(float(edge.get(attr)))
+    return values
+
 def extractMeanFeature(xml,feature, detectors):
     tree = ET.parse(xml)
     root = tree.getroot()
@@ -36,17 +51,33 @@ def extractAndPlot(xml, feature):
     time_range = list(data.keys())
     values = list(data.values())
 
-    for d in range(len(detectors)):
-        vals = [v[d] for v in values]
-        label = '{} {}'.format(feature, detectors[d])
-        plt.plot(time_range, vals, label=label)
+    flows = extractMeanFeature(xml, 'flow', detectors).values()
+    densities = np.array(list(values)) / np.array(list(flows))
+
+    mean = np.average(values, weights=densities, axis=1)
+
+    # for d in range(len(detectors)):
+    #     vals = [v[d] for v in values]
+    #label = '{} {}'.format(feature, detectors[d])
+
+    plt.plot(time_range, mean, label=feature)
+    return time_range
+
+def extractAndPlotMeanSpaceSpeed(xml, xs):
+    xml = ET.parse(file2)
+    space_mean_speed = fetch_edge_attrs(xml.getroot(), 'link4', ['speed'])['speed']
+    plt.plot(xs, space_mean_speed, label="space mean speed", linestyle='dashed')
 
 if __name__ == "__main__":
     file = sys.argv[1]
 
     extractAndPlot(file, 'speed')
-    extractAndPlot(file, 'harmonicMeanSpeed')
-    plt.title('Average vs Harmonic Speed - Edge 4 (loop in the end)')
+    time_range = extractAndPlot(file, 'harmonicMeanSpeed')
+
+    file2 = sys.argv[2]
+    extractAndPlotMeanSpaceSpeed(file2, time_range)
+
+    plt.title('Average vs Harmonic Speed - Edge 4 (loop in the middle)')
     plt.xlabel('Duration (minutes)')
     plt.ylabel('Speed (m/s)')
     plt.grid(True)
