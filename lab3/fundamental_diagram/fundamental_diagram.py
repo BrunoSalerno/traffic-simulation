Fix speed units and try Greenberg

lab3/fundamental_diagram/fundamental_diagram.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 import sys
 import numpy as np
 import sklearn.metrics
+import scipy.optimize
 
 def fetch_edge_attrs(data, edge_id, attrs):
     values = {}
@@ -23,7 +24,7 @@ def fetch_edge_attrs(data, edge_id, attrs):
     return values
 
 def greenshield(vals):
-    # The fit is a linear model, following Greenshield
+    # The fit is a linear model, following Greenshields
     # y(x) = a + b1.x
     # and
     # v(p) = vf(1 - p/pj)
@@ -35,13 +36,31 @@ def greenshield(vals):
     x = np.linspace(0,-a/b1)
     y = a + b1 * x
 
+    print('Greenshield: a: {} b1: {}'.format(a, b1))
+
     # Equal to y, but only for the actual speeds of x
     y_predicted = a + b1 * np.array(vals['density'])
 
     r2 = sklearn.metrics.r2_score(vals['speed'], y_predicted)
     return x,y,r2
 
+def _greenberg_func(p, pj):
+    v0 = 70
+    return v0 * np.log(pj/p)
 
+def greenberg(vals):
+    popt, pconv = scipy.optimize.curve_fit(_greenberg_func, vals['density'], vals['speed'])
+    pj = popt[0]
+    x = np.linspace(50,350)
+    y = _greenberg_func(x, pj)
+
+    print('Greenberg: pj: {}'.format(pj))
+
+    # Equal to y, but only for the actual speeds of x
+    y_predicted = _greenberg_func(np.array(vals['density']), pj)
+    r2 = sklearn.metrics.r2_score(vals['speed'], y_predicted)
+
+    return x, y, r2
 
 if __name__ == "__main__":
     filename = sys.argv[1]
@@ -52,16 +71,22 @@ if __name__ == "__main__":
     # According to https://sumo.dlr.de/docs/Simulation/Output/Lane-_or_Edge-based_Traffic_Measures.html,
     # `speed` "is an average over time and space (space-mean-speed)".
     vals = fetch_edge_attrs(xml.getroot(), edge, ['speed', 'density'])
+    # We convert speeds to km/h:
+    vals['speed'] = np.array(vals['speed']) * 3.6
+
     print(vals)
 
-    plt.scatter(vals['density'], vals['speed'])
+    plt.scatter(vals['density'], vals['speed'], color='gray')
 
     x,y,r2 = greenshield(vals)
-    plt.plot(x, y, color='black', label='Greenshield R2: {}'.format(round(r2,2)))
-    plt.axis(xmin=0,ymin=0)
+    plt.plot(x, y, label='Greenshields R2: {}'.format(round(r2,2)))
 
+    #x,y,r2 = greenberg(vals)
+    #plt.plot(x, y, label='Greenberg R2: {}'.format(round(r2,2)))
+
+    plt.axis(xmin=0,ymin=0)
     plt.xlabel('Density (#veh/km)')
-    plt.ylabel('Speed (m/s)')
+    plt.ylabel('Speed (km/h)')
     plt.title('{}: Speed vs Density (60 min)'.format(edge))
     plt.legend()
     plt.show()